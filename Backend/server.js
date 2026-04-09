eb771b4f9fdbd530a7f0eec87ccb2befff9440e9
Nileshnhe <nehenilesh111@gmail.com>
2026-04-09 11:20:59 +0530

    feat: update dependencies and restructure server initialization

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,25 @@
 import dotenv from "dotenv"
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`server os running on Port ${PORT}`)
-})
+const startServer = async () => {
+
+    try {
+        await connectDB()
+
+        app.listen(PORT, () => {
+            console.log(`server os running on Port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.error("Failed to start server", error.message);
+        process.exit(1);
+    }
+
+};
+
+startServer();
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,14 @@
 import express from "express"
-const app = express()
+import cookieParser from "cookie-parser"
+import morgan from "morgan"
 
-app.get("/health", (req, res) => {
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
     res.status(200).json({ message: "Server is running"})
 })
 
