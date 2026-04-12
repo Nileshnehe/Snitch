f56c51f40f287c509f6b5b1cc7c183731cfef37a
Nileshnhe <nehenilesh111@gmail.com>
2026-04-12 09:44:55 +0530

    feat: implement react router and define application routes

--- Frontend/src/app/App.jsx
+++ Frontend/src/app/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css"
+import { RouterProvider } from "react-router"
+import { routes } from "./app.routes"
 
 const App = () => {
   return (
 <>
-
+<RouterProvider router={routes} />
 </>  )
 }
 
