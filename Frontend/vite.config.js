d9fd6d47f3d95bf19cfebcafd926fe380075093b
Nileshnhe <nehenilesh111@gmail.com>
2026-04-23 22:35:33 +0530

    fix: update API proxy target to use production URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server:{
     proxy:{
       "/api":{
-        target: "http://localhost:3000",
+        target: "https://snitch-wr2l.onrender.com",
         changeOrigin: true,
         secure: false
       }
